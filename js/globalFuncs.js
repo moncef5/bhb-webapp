aa86cb3868cef876689a89422bf9c3c77acd514b
DavidArthurCole <40234707+DavidArthurCole@users.noreply.github.com>
2022-11-04 20:35:14 -0400

    Fix cookie logging issue

--- js/globalFuncs.js
+++ js/globalFuncs.js
@@ -107,13 +107,13 @@ function deleteAllCookies() {
     for (const element of cookies) {
         var d = window.location.hostname.split(".");
         while (d.length > 0) {
-            var cookieBase = encodeURIComponent(element.split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
+            var cookieBase = encodeURIComponent(element.split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT;path=';
             var p = location.pathname.split('/');
             document.cookie = cookieBase + '/';
             while (p.length > 0) {
                 document.cookie = cookieBase + p.join('/');
                 p.pop();
-            };
+            }
             d.shift();
         }
     }
