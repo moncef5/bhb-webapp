331974e4b43bc6fa28515aa62af0546a285c0384
DavidArthurCole <davidarthurcole23@gmail.com>
2023-08-17 21:46:51 -0400

    Change to shipping no .js files, that's yucky - now it's just 3 script tags, idk. That might be worse

--- js/bhbScript.js
+++
@@ -1,8 +0,0 @@
-//Set default values for color pickers
-Array.from(document.getElementsByClassName("color_picker")).forEach(colorPicker => colorPicker.value = (darkMode ? "#000000" : "#FFFFFF"));
-//Set default values for code inputs
-Array.from(document.getElementsByClassName("code_input")).forEach(codeInput => codeInput.dispatchEvent(new Event('change')));
-//Unlock code fields, where applicable
-unlockFields();
-//Check inputs on BHB
-checkInput();
